4167885d7a8ac3bd00b95c971174136fbe666c9b
jkdihenkar <jkdihenkar@gmail.com>
2018-12-11 21:17:29 +0530

    initial skel from start springboot

---
+++ src/main/java/com/example/learningspringbootjd/LearningspringbootjdApplication.java
@@ -0,0 +1,12 @@
+package com.example.learningspringbootjd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningspringbootjdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningspringbootjdApplication.class, args);
+	}
+}
