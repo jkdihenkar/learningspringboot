1423f19749344178d05e9ff38cf30669b2ba97f2
jkdihenkar <jkdihenkar@gmail.com>
2018-12-14 20:49:19 +0530

    auto-config, config values and jetty server

--- src/main/java/com/example/learningspringbootjd/LearningspringbootjdApplication.java
+++ src/main/java/com/example/learningspringbootjd/LearningspringbootjdApplication.java
@@ -1,11 +1,13 @@
 package com.example.learningspringbootjd;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({"simplecontrollers"})
+@EnableAutoConfiguration
 public class LearningspringbootjdApplication {
 
 	public static void main(String[] args) {
--- src/main/java/simplecontrollers/HelloWorldController.java
+++ src/main/java/simplecontrollers/HelloWorldController.java
@@ -1,5 +1,6 @@
 package simplecontrollers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController(value = "/api")
 public class HelloWorldController {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
 
     @GetMapping("/v1")
     public HelloWorld sayHelloReqParam(@RequestParam(name = "content", defaultValue = "Hello World!") String message) {
@@ -17,5 +21,11 @@ public class HelloWorldController {
     public HelloWorld sayHelloPathVar(@PathVariable(name = "content") String message) {
         return new HelloWorld(1, message);
     }
+
+    @GetMapping("/v1/appname")
+    public HelloWorld sayName() {
+        return new HelloWorld(1, applicationName);
+    }
+
 
 }
